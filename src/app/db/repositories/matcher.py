95d6b6fd0426eaeb86b211f85e24065836acc6c1
faris361707@gmail.com <faris361707@gmail.com>
2025-06-14 20:36:03 +0700

    feat : aho corassick

--- src/app/db/repositories/matcher.py
+++ src/app/db/repositories/matcher.py
@@ -1,13 +1,213 @@
 import os
 import re
-from collections import Counter, deque
-from typing import List, Dict, Tuple, Union
+from typing import List, Dict, Union
 import fitz
+
+# SOURCE : https://www.geeksforgeeks.org/dsa/aho-corasick-algorithm-pattern-searching/
+class AhoCorasick:
+    def __init__(self, words):
+
+        # Max number of states in the matching machine.
+        # Should be equal to the sum of the length of all keywords.
+        self.max_states = sum([len(word) for word in words])
+
+        # Maximum number of characters.
+        # Currently supports only alphabets [a,z]
+        self.max_characters = 37
+
+        # OUTPUT FUNCTION IS IMPLEMENTED USING out []
+        # Bit i in this mask is 1 if the word with
+        # index i appears when the machine enters this state.
+        # Lets say, a state outputs two words "he" and "she" and
+        # in our provided words list, he has index 0 and she has index 3
+        # so value of out[state] for this state will be 1001
+        # It has been initialized to all 0.
+        # We have taken one extra state for the root.
+        self.out = [0]*(self.max_states+1)
+
+        # FAILURE FUNCTION IS IMPLEMENTED USING fail []
+        # There is one value for each state + 1 for the root
+        # It has been initialized to all -1
+        # This will contain the fail state value for each state
+        self.fail = [-1]*(self.max_states+1)
+
+        # GOTO FUNCTION (OR TRIE) IS IMPLEMENTED USING goto [[]]
+        # Number of rows = max_states + 1
+        # Number of columns = max_characters i.e 26 in our case
+        # It has been initialized to all -1.
+        self.goto = [[-1]*self.max_characters for _ in range(self.max_states+1)]
+        
+        # Convert all words to lowercase
+        # so that our search is case insensitive
+        for i in range(len(words)):
+          words[i] = words[i].lower()
+          
+        # All the words in dictionary which will be used to create Trie
+        # The index of each keyword is important:
+        # "out[state] & (1 << i)" is > 0 if we just found word[i]
+        # in the text.
+        self.words = words
+
+        # Once the Trie has been built, it will contain the number
+        # of nodes in Trie which is total number of states required <= max_states
+        self.states_count = self.__build_matching_machine()
+
+
+    # Builds the String matching machine.
+    # Returns the number of states that the built machine has.
+    # States are numbered 0 up to the return value - 1, inclusive.
+    def __build_matching_machine(self):
+        k = len(self.words)
+
+        # Initially, we just have the 0 state
+        states = 1
+
+        # Convalues for goto function, i.e., fill goto
+        # This is same as building a Trie for words[]
+        for i in range(k):
+            word = self.words[i]
+            current_state = 0
+
+            # Process all the characters of the current word
+            for character in word:
+                ch = None
+                if (character.isalpha()):
+                    ch = ord(character.lower()) - 97
+                elif (character.isdigit()):
+                    ch = ord(character) - 22
+
+                # Allocate a new node (create a new state)
+                # if a node for ch doesn't exist.
+                if self.goto[current_state][ch] == -1:
+                    self.goto[current_state][ch] = states
+                    states += 1
+
+                current_state = self.goto[current_state][ch]
+
+            # Add current word in output function
+            self.out[current_state] |= (1<<i)
+
+        # For all characters which don't have
+        # an edge from root (or state 0) in Trie,
+        # add a goto edge to state 0 itself
+        for ch in range(self.max_characters):
+            if self.goto[0][ch] == -1:
+                self.goto[0][ch] = 0
+        
+        # Failure function is computed in 
+        # breadth first order using a queue
+        queue = []
+
+        # Iterate over every possible input
+        for ch in range(self.max_characters):
+
+            # All nodes of depth 1 have failure
+            # function value as 0. For example,
+            # in above diagram we move to 0
+            # from states 1 and 3.
+            if self.goto[0][ch] != 0:
+                self.fail[self.goto[0][ch]] = 0
+                queue.append(self.goto[0][ch])
+
+        # Now queue has states 1 and 3
+        while queue:
+
+            # Remove the front state from queue
+            state = queue.pop(0)
+
+            # For the removed state, find failure
+            # function for all those characters
+            # for which goto function is not defined.
+            for ch in range(self.max_characters):
+
+                # If goto function is defined for
+                # character 'ch' and 'state'
+                if self.goto[state][ch] != -1:
+
+                    # Find failure state of removed state
+                    failure = self.fail[state]
+
+                    # Find the deepest node labeled by proper
+                    # suffix of String from root to current state.
+                    while self.goto[failure][ch] == -1:
+                        failure = self.fail[failure]
+                    
+                    failure = self.goto[failure][ch]
+                    self.fail[self.goto[state][ch]] = failure
+
+                    # Merge output values
+                    self.out[self.goto[state][ch]] |= self.out[failure]
+
+                    # Insert the next level node (of Trie) in Queue
+                    queue.append(self.goto[state][ch])
+        
+        return states
+
+
+    # Returns the next state the machine will transition to using goto
+    # and failure functions.
+    # current_state - The current state of the machine. Must be between
+    #             0 and the number of states - 1, inclusive.
+    # next_input - The next character that enters into the machine.
+    def __find_next_state(self, current_state, next_input):
+        answer = current_state
+        ch = None
+        if (next_input.isalpha()):
+            ch = ord(next_input.lower()) - 97
+        elif (next_input.isdigit()):
+            ch = ord(next_input) - 22
+        else:
+            ch = 36
+
+        # If goto is not defined, use
+        # failure function
+        while self.goto[answer][ch] == -1:
+            answer = self.fail[answer]
+
+        return self.goto[answer][ch]
+
+
+    # This function finds all occurrences of all words in text.
+    def search_words(self, text) -> Dict:
+        # Convert the text to lowercase to make search case insensitive
+
+        # Initialize current_state to 0 
+        current_state = 0
+
+        # A dictionary to store the result.
+        # Key here is the found word
+        # Value is a list of all occurrences start index
+        results = [0] * len(self.words)
+
+        # Traverse the text through the built machine
+        # to find all occurrences of words
+        for i in range(len(text)):
+            current_state = self.__find_next_state(current_state, text[i])
+
+            # If match not found, move to next state
+            if self.out[current_state] == 0: continue
+
+            # Match found, store the word in result dictionary
+            for j in range(len(self.words)):
+                if (self.out[current_state] & (1<<j)) > 0:
+                    results[j] += 1
+
+        total_queries = len(self.words)
+        queries = self.words
+
+        # Return the final result dictionary
+        return {
+            'total_queries': total_queries,
+            'query': queries,
+            'matched_queries': results,
+            'method_used': "Aho-Corasick",
+        }
 
 class Matcher:
     def __init__(self, cv_paths: List[str], queries: List[str]):
         self.cv_paths = cv_paths
         self.texts = []
+        self.automaton_trie = None
 
         self.queries = [query.lower() for query in queries]
 
@@ -30,6 +230,7 @@ class Matcher:
             else:
                 raise ValueError(f"Unsupported file format: {file_ext}")
             
+            text = re.sub(r'[^a-zA-Z0-9\s]', '', text)
             result = text.lower() if case == 0 else text
             print(f"Finished extracting text ({'lowercase' if case == 0 else 'original case'})")
             return result
@@ -73,7 +274,11 @@ class Matcher:
                 result.append(self._BM_match(text, self.queries))
             elif method == 'AC':
                 # TODO: Implement Aho-Corasick algorithm
-                print("Aho-Corasick algorithm is not implemented yet.")
+
+                if (self.automaton_trie is None):
+                    # build the automaton trie here
+                    automaton_trie = AhoCorasick(self.queries)
+                result.append(automaton_trie.search_words(text))
             elif method == 'fuzzy':
                 result.append(self._fuzzy_match(text, self.queries, threshold))
             else:
@@ -231,8 +436,7 @@ class Matcher:
         assert threshold >= 0 and threshold <= 1, "Threshold must be between 0 and 1"
 
         """Fuzzy matching using simple similarity calculation"""
-        text
-        
+
         matches = []
         count = 0
         
@@ -266,8 +470,9 @@ class Matcher:
         }
 
 if __name__ == "__main__":
-    matcher = Matcher(["10554236.pdf"], ["financel"])
+    matcher = Matcher(["10554236.pdf"], ["financial", "other"])
     print(matcher.match("KMP"))
     print(matcher.match("BM"))
     print(matcher.match("exact"))
     print(matcher.match("fuzzy", threshold=0.6))
+    print(matcher.match("AC"))
